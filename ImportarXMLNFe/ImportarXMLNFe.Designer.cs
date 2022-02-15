04239a481244cfe01db2eba5a47dc0ba10f620cc
leand <leand@LEANDRO>
2022-02-14 21:24:33 -0300

    Adicionar arquivos de projeto.

---
+++ ImportarXMLNFe/ImportarXMLNFe.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ImportarXMLNFe
+{
+    partial class FormImportarXMLNFe
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLerFolderXml = new System.Windows.Forms.Button();
+            this.txtpathFolderXml = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnLerFolderXml
+            // 
+            this.btnLerFolderXml.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLerFolderXml.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLerFolderXml.Location = new System.Drawing.Point(420, 12);
+            this.btnLerFolderXml.Name = "btnLerFolderXml";
+            this.btnLerFolderXml.Size = new System.Drawing.Size(32, 23);
+            this.btnLerFolderXml.TabIndex = 3;
+            this.btnLerFolderXml.Text = "...";
+            this.btnLerFolderXml.UseVisualStyleBackColor = true;
+            this.btnLerFolderXml.Click += new System.EventHandler(this.btnLerFolderXml_Click);
+            // 
+            // txtpathFolderXml
+            // 
+            this.txtpathFolderXml.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtpathFolderXml.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpathFolderXml.Location = new System.Drawing.Point(12, 12);
+            this.txtpathFolderXml.Name = "txtpathFolderXml";
+            this.txtpathFolderXml.ReadOnly = true;
+            this.txtpathFolderXml.Size = new System.Drawing.Size(402, 23);
+            this.txtpathFolderXml.TabIndex = 2;
+            // 
+            // FormImportarXMLNFe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLerFolderXml);
+            this.Controls.Add(this.txtpathFolderXml);
+            this.Name = "FormImportarXMLNFe";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLerFolderXml;
+        private System.Windows.Forms.TextBox txtpathFolderXml;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ ImportarXMLNFe/ImportarXMLNFe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImportarXMLNFe
+{
+    public partial class FormImportarXMLNFe : Form
+    {
+        public FormImportarXMLNFe()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLerFolderXml_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    txtpathFolderXml.Text = folderBrowserDialog1.SelectedPath;
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Falha no processo de leitura do arquivo xml da Nota Fiscal.", "Aviso - Leitura do Arquivo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
