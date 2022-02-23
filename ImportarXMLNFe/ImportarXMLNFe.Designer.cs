ad81335153b048df84870a40169fa4c583609363
Leandro Alves da Silva <leandroalves.desenvolvedor@gmail.com>
2022-02-22 23:57:01 -0300

    #08 - Ajuste para imprimir pdf e detalhe

--- ImportarXMLNFe/ImportarXMLNFe.Designer.cs
+++ ImportarXMLNFe/ImportarXMLNFe.Designer.cs
@@ -28,45 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.btnLerFolderXml = new System.Windows.Forms.Button();
-            this.txtpathFolderXml = new System.Windows.Forms.TextBox();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.dataGridView = new System.Windows.Forms.DataGridView();
             this.nNF = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dhEmi = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.vNF = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Path = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.xNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CpfCnpj = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.xMun = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.UF = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CEP = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Path = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grpCaminho = new System.Windows.Forms.GroupBox();
+            this.btnLerFolderXml = new System.Windows.Forms.Button();
+            this.txtpathFolderXml = new System.Windows.Forms.TextBox();
+            this.grpTipoVisualizacao = new System.Windows.Forms.GroupBox();
+            this.chkVisualizarPdf = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.grpCaminho.SuspendLayout();
+            this.grpTipoVisualizacao.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnLerFolderXml
-            // 
-            this.btnLerFolderXml.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLerFolderXml.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLerFolderXml.Location = new System.Drawing.Point(561, 12);
-            this.btnLerFolderXml.Name = "btnLerFolderXml";
-            this.btnLerFolderXml.Size = new System.Drawing.Size(32, 23);
-            this.btnLerFolderXml.TabIndex = 2;
-            this.btnLerFolderXml.Text = "...";
-            this.btnLerFolderXml.UseVisualStyleBackColor = true;
-            this.btnLerFolderXml.Click += new System.EventHandler(this.btnLerFolderXml_Click);
-            // 
-            // txtpathFolderXml
-            // 
-            this.txtpathFolderXml.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtpathFolderXml.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtpathFolderXml.Location = new System.Drawing.Point(12, 12);
-            this.txtpathFolderXml.Name = "txtpathFolderXml";
-            this.txtpathFolderXml.ReadOnly = true;
-            this.txtpathFolderXml.Size = new System.Drawing.Size(543, 23);
-            this.txtpathFolderXml.TabIndex = 1;
             // 
             // dataGridView
             // 
@@ -75,36 +58,36 @@
             this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.nNF,
             this.dhEmi,
             this.vNF,
+            this.Path,
             this.xNome,
             this.CpfCnpj,
             this.xMun,
             this.UF,
-            this.CEP,
-            this.Path});
+            this.CEP});
             this.dataGridView.EnableHeadersVisualStyles = false;
-            this.dataGridView.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView.Location = new System.Drawing.Point(12, 70);
             this.dataGridView.MultiSelect = false;
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.ReadOnly = true;
             this.dataGridView.RowHeadersVisible = false;
             this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView.Size = new System.Drawing.Size(1010, 508);
+            this.dataGridView.Size = new System.Drawing.Size(1010, 479);
             this.dataGridView.TabIndex = 3;
             this.dataGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellDoubleClick);
-            this.dataGridView.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dataGridView_KeyPress);
+            this.dataGridView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView_KeyDown);
             // 
             // nNF
             // 
@@ -121,13 +104,20 @@
             // 
             // vNF
             // 
-            dataGridViewCellStyle2.Format = "C2";
-            dataGridViewCellStyle2.NullValue = null;
-            this.vNF.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle6.Format = "C2";
+            dataGridViewCellStyle6.NullValue = null;
+            this.vNF.DefaultCellStyle = dataGridViewCellStyle6;
             this.vNF.HeaderText = "Valor Nota Fiscal";
             this.vNF.Name = "vNF";
             this.vNF.ReadOnly = true;
             this.vNF.Width = 120;
+            // 
+            // Path
+            // 
+            this.Path.HeaderText = "Path";
+            this.Path.Name = "Path";
+            this.Path.ReadOnly = true;
+            this.Path.Visible = false;
             // 
             // xNome
             // 
@@ -162,45 +152,96 @@
             this.CEP.Name = "CEP";
             this.CEP.ReadOnly = true;
             // 
-            // Path
+            // grpCaminho
             // 
-            this.Path.HeaderText = "Path";
-            this.Path.Name = "Path";
-            this.Path.ReadOnly = true;
-            this.Path.Visible = false;
+            this.grpCaminho.Controls.Add(this.btnLerFolderXml);
+            this.grpCaminho.Controls.Add(this.txtpathFolderXml);
+            this.grpCaminho.Location = new System.Drawing.Point(12, 12);
+            this.grpCaminho.Name = "grpCaminho";
+            this.grpCaminho.Size = new System.Drawing.Size(600, 52);
+            this.grpCaminho.TabIndex = 4;
+            this.grpCaminho.TabStop = false;
+            this.grpCaminho.Text = "Caminho dos arquivos";
+            // 
+            // btnLerFolderXml
+            // 
+            this.btnLerFolderXml.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLerFolderXml.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLerFolderXml.Location = new System.Drawing.Point(555, 19);
+            this.btnLerFolderXml.Name = "btnLerFolderXml";
+            this.btnLerFolderXml.Size = new System.Drawing.Size(32, 23);
+            this.btnLerFolderXml.TabIndex = 4;
+            this.btnLerFolderXml.Text = "...";
+            this.btnLerFolderXml.UseVisualStyleBackColor = true;
+            this.btnLerFolderXml.Click += new System.EventHandler(this.btnLerFolderXml_Click);
+            // 
+            // txtpathFolderXml
+            // 
+            this.txtpathFolderXml.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtpathFolderXml.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpathFolderXml.Location = new System.Drawing.Point(6, 19);
+            this.txtpathFolderXml.Name = "txtpathFolderXml";
+            this.txtpathFolderXml.ReadOnly = true;
+            this.txtpathFolderXml.Size = new System.Drawing.Size(543, 23);
+            this.txtpathFolderXml.TabIndex = 3;
+            // 
+            // grpTipoVisualizacao
+            // 
+            this.grpTipoVisualizacao.Controls.Add(this.chkVisualizarPdf);
+            this.grpTipoVisualizacao.Location = new System.Drawing.Point(618, 12);
+            this.grpTipoVisualizacao.Name = "grpTipoVisualizacao";
+            this.grpTipoVisualizacao.Size = new System.Drawing.Size(404, 52);
+            this.grpTipoVisualizacao.TabIndex = 5;
+            this.grpTipoVisualizacao.TabStop = false;
+            this.grpTipoVisualizacao.Text = "Tipo de Visualização";
+            // 
+            // chkVisualizarPdf
+            // 
+            this.chkVisualizarPdf.AutoSize = true;
+            this.chkVisualizarPdf.Location = new System.Drawing.Point(7, 24);
+            this.chkVisualizarPdf.Name = "chkVisualizarPdf";
+            this.chkVisualizarPdf.Size = new System.Drawing.Size(47, 17);
+            this.chkVisualizarPdf.TabIndex = 5;
+            this.chkVisualizarPdf.Text = "PDF";
+            this.chkVisualizarPdf.UseVisualStyleBackColor = true;
             // 
             // FormImportarXMLNFe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1034, 561);
+            this.Controls.Add(this.grpTipoVisualizacao);
+            this.Controls.Add(this.grpCaminho);
             this.Controls.Add(this.dataGridView);
-            this.Controls.Add(this.btnLerFolderXml);
-            this.Controls.Add(this.txtpathFolderXml);
             this.Name = "FormImportarXMLNFe";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Importar XML NFe/NFC-e";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.grpCaminho.ResumeLayout(false);
+            this.grpCaminho.PerformLayout();
+            this.grpTipoVisualizacao.ResumeLayout(false);
+            this.grpTipoVisualizacao.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnLerFolderXml;
-        private System.Windows.Forms.TextBox txtpathFolderXml;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         public System.Windows.Forms.DataGridView dataGridView;
         private System.Windows.Forms.DataGridViewTextBoxColumn nNF;
         private System.Windows.Forms.DataGridViewTextBoxColumn dhEmi;
         private System.Windows.Forms.DataGridViewTextBoxColumn vNF;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Path;
         private System.Windows.Forms.DataGridViewTextBoxColumn xNome;
         private System.Windows.Forms.DataGridViewTextBoxColumn CpfCnpj;
         private System.Windows.Forms.DataGridViewTextBoxColumn xMun;
         private System.Windows.Forms.DataGridViewTextBoxColumn UF;
         private System.Windows.Forms.DataGridViewTextBoxColumn CEP;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Path;
+        private System.Windows.Forms.GroupBox grpCaminho;
+        private System.Windows.Forms.Button btnLerFolderXml;
+        private System.Windows.Forms.TextBox txtpathFolderXml;
+        private System.Windows.Forms.GroupBox grpTipoVisualizacao;
+        private System.Windows.Forms.CheckBox chkVisualizarPdf;
     }
 }
 
--- ImportarXMLNFe/ImportarXMLNFe.cs
+++ ImportarXMLNFe/ImportarXMLNFe.cs
@@ -1,23 +1,97 @@
-﻿using ImportarXMLNFe.Model;
+﻿using ACBrLib.NFe;
+using ImportarXMLNFe.Model;
 using ImportarXMLNFe.Serializable;
-using PdfiumViewer;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
-using ZionDanfe;
-using ZionDanfe.Modelo;
 
 namespace ImportarXMLNFe
 {
     public partial class FormImportarXMLNFe : Form
     {
+        private ACBrNFe ACBrNFe;
+        List<NFeProc> listaNFeProc = new List<NFeProc>();
         public FormImportarXMLNFe()
         {
             InitializeComponent();
+            ACBrNFe = new ACBrNFe();
         }
+        public void AtualizarGrid(NFeProc nfe, string path)
+        {
+            if (nfe != null)
+            {
+                listaNFeProc.Add(nfe);
+                if (nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario == null)
+                {
+                    dataGridView.Rows.Add(nfe.NotaFiscalEletronica.InformacoesNFe.Identificacao.nNF,
+                                          nfe.NotaFiscalEletronica.InformacoesNFe.Identificacao.dhEmi.ToShortDateString(),
+                                          nfe.NotaFiscalEletronica.InformacoesNFe.Total.ICMSTotal.vNF,
+                                          path);
+                }
+                else if (nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.Endereco == null)
+                {
+                    dataGridView.Rows.Add(nfe.NotaFiscalEletronica.InformacoesNFe.Identificacao.nNF,
+                                          nfe.NotaFiscalEletronica.InformacoesNFe.Identificacao.dhEmi.ToShortDateString(),
+                                          nfe.NotaFiscalEletronica.InformacoesNFe.Total.ICMSTotal.vNF, path,
+                                          nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.xNome,
+                                          string.IsNullOrEmpty(nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.CNPJ) ? nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.CPF : nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.CNPJ);
+                }
+                else
+                {
+                    dataGridView.Rows.Add(nfe.NotaFiscalEletronica.InformacoesNFe.Identificacao.nNF,
+                                          nfe.NotaFiscalEletronica.InformacoesNFe.Identificacao.dhEmi.ToShortDateString(),
+                                          nfe.NotaFiscalEletronica.InformacoesNFe.Total.ICMSTotal.vNF, path,
+                                          nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.xNome,
+                                          string.IsNullOrEmpty(nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.CNPJ) ? nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.CPF : nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.CNPJ,
+                                          nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.Endereco.xMun,
+                                          nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.Endereco.UF,
+                                          nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.Endereco.CEP);
+                }
+            }
+        }
+        private void dataGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView.Rows.Count > 0 && dataGridView.SelectedRows.Count > 0)
+            {
+                if (chkVisualizarPdf.Checked)
+                {
+                    ACBrNFe.LimparLista();
+                    ACBrNFe.CarregarXML(dataGridView.SelectedRows[0].Cells[3].Value.ToString());
+                    ACBrNFe.Imprimir(bMostrarPreview: true);
+                }
+                else
+                {
+                    var nfe = listaNFeProc.Find(x => x.NotaFiscalEletronica.InformacoesNFe.Identificacao.nNF == dataGridView.SelectedRows[0].Cells[0].Value);
 
-        List<NFeProc> listaNFeProc = new List<NFeProc>();
+                    DetalheXMLNFe detalhe = new DetalheXMLNFe(nfe);
+                    detalhe.ShowDialog();
+                }
+            }
+        }
+        private void dataGridView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) { 
+                e.Handled = true;
+                if (dataGridView.Rows.Count > 0 && dataGridView.SelectedRows.Count > 0)
+                {
+                    if (chkVisualizarPdf.Checked)
+                    {
+                        ACBrNFe.LimparLista();
+                        ACBrNFe.CarregarXML(dataGridView.SelectedRows[0].Cells[3].Value.ToString());
+                        ACBrNFe.Imprimir(bMostrarPreview: true);
+                    }
+                    else
+                    {
+                        var nfe = listaNFeProc.Find(x => x.NotaFiscalEletronica.InformacoesNFe.Identificacao.nNF == dataGridView.SelectedRows[0].Cells[0].Value);
+
+                        DetalheXMLNFe detalhe = new DetalheXMLNFe(nfe);
+                        detalhe.ShowDialog();
+                    }
+
+                }
+            } 
+        }
 
         private void btnLerFolderXml_Click(object sender, EventArgs e)
         {
@@ -35,73 +109,13 @@ namespace ImportarXMLNFe
                     listaNFeProc.Clear();
                     foreach (FileInfo fileinfo in Arquivos)
                     {
-                         AtualizarGrid(serializable.GetObjectFromFile<NFeProc>(txtpathFolderXml.Text + "\\" + fileinfo.Name), txtpathFolderXml.Text + "\\" + fileinfo.Name);
+                        AtualizarGrid(serializable.GetObjectFromFile<NFeProc>(txtpathFolderXml.Text + "\\" + fileinfo.Name), txtpathFolderXml.Text + "\\" + fileinfo.Name);
                     }
                 }
             }
             catch (Exception)
             {
                 MessageBox.Show("Falha no processo de leitura do arquivo xml da Nota Fiscal.", "Aviso - Leitura do Arquivo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-        }
-
-        public void AtualizarGrid(NFeProc nfe, string path)
-        {
-            if (nfe != null)
-            {
-                listaNFeProc.Add(nfe);
-
-                if (nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario == null)
-                {
-                    dataGridView.Rows.Add(nfe.NotaFiscalEletronica.InformacoesNFe.Identificacao.nNF,
-                                          nfe.NotaFiscalEletronica.InformacoesNFe.Identificacao.dhEmi.ToShortDateString(),
-                                          nfe.NotaFiscalEletronica.InformacoesNFe.Total.ICMSTotal.vNF,
-                                          path);
-                }
-                else if (nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.Endereco == null)
-                {
-                    dataGridView.Rows.Add(nfe.NotaFiscalEletronica.InformacoesNFe.Identificacao.nNF,
-                                          nfe.NotaFiscalEletronica.InformacoesNFe.Identificacao.dhEmi.ToShortDateString(),
-                                          nfe.NotaFiscalEletronica.InformacoesNFe.Total.ICMSTotal.vNF,
-                                          nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.xNome,
-                                          string.IsNullOrEmpty(nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.CNPJ) ? nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.CPF : nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.CNPJ,
-                                          path);
-                }
-                else
-                {
-                    dataGridView.Rows.Add(nfe.NotaFiscalEletronica.InformacoesNFe.Identificacao.nNF,
-                                          nfe.NotaFiscalEletronica.InformacoesNFe.Identificacao.dhEmi.ToShortDateString(),
-                                          nfe.NotaFiscalEletronica.InformacoesNFe.Total.ICMSTotal.vNF,
-                                          nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.xNome,
-                                          string.IsNullOrEmpty(nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.CNPJ) ? nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.CPF : nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.CNPJ,
-                                          nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.Endereco.xMun,
-                                          nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.Endereco.UF,
-                                          nfe.NotaFiscalEletronica.InformacoesNFe.Destinatario.Endereco.CEP,
-                                          path);
-                }
-            }
-        }
-
-        private void dataGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (dataGridView.Rows.Count > 0 && dataGridView.SelectedRows.Count > 0)
-            {
-                PrintXML printXML = new PrintXML(dataGridView.SelectedRows[0].Cells[8].Value.ToString());
-                printXML.ShowDialog();
-            }
-        }
-
-        private void dataGridView_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (e.KeyChar == 13)
-            {
-                if (dataGridView.Rows.Count > 0 && dataGridView.SelectedRows.Count > 0)
-                {
-                    var nfe = listaNFeProc.Find(x => x.NotaFiscalEletronica.InformacoesNFe.Identificacao.nNF == dataGridView.SelectedRows[0].Cells[0].Value);
-
-                    DetalheXMLNFe detalhe = new DetalheXMLNFe(nfe);
-                    detalhe.ShowDialog();
-                }
             }
         }
     }
